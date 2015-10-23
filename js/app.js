73a2a2c0543ef8e5a1e1662a9b6fe15adea75070
Ben S <allobon@gmail.com>
2015-10-23 16:56:48 +0100

    gettin there

--- js/app.js
+++ js/app.js
@@ -1,8 +1,26 @@
+var map;
+
+function loadMapsAPI()
+{
+	map = new google.maps.Map(document.getElementById('skate-map'), {
+    	center: {lat: 53.869065, lng: -2.394535},
+		zoom: 11
+	});
+
+
+}
+
 $(document).ready(function(){
+
 	$(".nav-item").click(function(){
 		var which = event.target.id;
 		turnDownLights(which);
 	});
+
+	google.maps.visualRefresh = true;
+
+	loadMapsAPI();
+
 });
 
 function turnDownLights(which)
@@ -22,6 +40,7 @@ function turnDownLights(which)
 		case "skateMap":
 			$(".skate-map-overlay").fadeIn(function(){
 				$(".skate-map").fadeIn();
+				loadMapsAPI()
 			});
 		break;
 
