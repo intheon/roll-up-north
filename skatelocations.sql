59f897e1ec04770d34481ae7a319e2cdb11a9974
intheon <allobon@gmail.com>
2015-11-11 21:36:09 +0000

    dwqdwq

--- skatelocations.sql
+++ skatelocations.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 05, 2015 at 01:05 AM
+-- Generation Time: Nov 11, 2015 at 10:35 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -36,14 +36,16 @@ CREATE TABLE IF NOT EXISTS `skatelocations` (
   `location_long` float(10,6) NOT NULL,
   `location_lat` float(10,6) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `skatelocations`
 --
 
 INSERT INTO `skatelocations` (`id`, `location_name`, `location_rating`, `location_description`, `adder_name`, `adder_ip`, `location_long`, `location_lat`) VALUES
-(1, 'Clitheroe Skatepark', 5, 'Clitheroe was built in 2006. Concrete Park with 2 minis, bowl, banks, pyramid, hubba, rails, kickers, manny pads, and ledges. It kicks ass!', 'Ben', '0.0.0.0', -2.394509, 53.869286);
+(1, 'Clitheroe Skatepark', 5, 'Clitheroe was built in 2006. Concrete Park with 2 minis, bowl, banks, pyramid, hubba, rails, kickers, manny pads, and ledges. It kicks ass!', 'Ben', '0.0.0.0', -2.394509, 53.869286),
+(3, 'Mile End', 5, 'Street park with smooth as hell concrete. This is where you get tech!', 'Ben', '::1', -0.030679, 51.517532),
+(4, 'Victoria Park', 4, 'The bowl is ridiculous', 'Ben', '::1', -0.034933, 51.538074);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
